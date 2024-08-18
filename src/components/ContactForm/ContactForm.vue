cd23410eab91cdcedc960b801092d4e808e54190
pavelbekarev <bekarev.ps@gmail.com>
2024-08-18 23:40:48 +0500

    added ContactForm component

---
+++ src/components/ContactForm/ContactForm.vue
@@ -0,0 +1,29 @@
+<script setup>
+    import "../../style/Contact.scss";
+    import { ref } from "vue";
+
+    // script
+    const nameValue = ref("");
+    const emailValue = ref("");
+    const topicValue = ref("");
+</script>
+
+<template>
+    <div class="form__booking">
+        <h2 class="booking__h2">Записаться на разбор</h2>
+        <div class="form__booking-data">
+            <div class="input__wrapper">
+                <label class="booking__label" for="#name">Ваше имя</label>
+                <input id="name" class="booking__input" type="text" v-model="nameValue">
+            </div>
+            <div class="input__wrapper">
+                <label class="booking__label" for="#email">Ваш email</label>
+                <input id="email" class="booking__input" type="text" v-model="emailValue">
+            </div>
+            <div class="input__wrapper">
+                <label class="booking__label" for="#topic">Тема</label>
+                <input id="topic" class="booking__input" type="text" v-model="topicValue">
+            </div>
+        </div>
+    </div>
+</template>
