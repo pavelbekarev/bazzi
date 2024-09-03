a3c5443456f86b530b6ba69824ee72ccccfbce33
pavelbekarev <bekarev.ps@gmail.com>
2024-09-03 19:46:31 +0500

    b

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: "https://github.com/pavelbekarev/bazzi"
+  publicPath: "https://pavelbekarev.github.io/bazzi/"
 })
