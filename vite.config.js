290d72141c2cd146069e4a7a444c455ffddfb233
pavelbekarev <bekarev.ps@gmail.com>
2024-09-04 14:31:01 +0500

    s

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicDir: "",
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/bazzi/'
+    : '/',
   plugins: [
     vue(),
   ],
